ac439605d79419f8991bca2e29ee3a4e7d85d695
Davi Cedraz <davioler@gmail.com>
2023-02-19 23:54:21 -0300

    feat: add security packages and some logs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { rateLimit } from 'express-rate-limit';
+import * as morgan from 'morgan';
+import helmet from 'helmet';
+
+function handleSecurity(app) {
+  app.use(helmet()); //middleware to set security headers
+  app.use(rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  }));
+}
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log(process.env.NODE_ENV);
+
+  handleSecurity(app);
+  app.use(morgan('tiny')); //enable http requests logs
+
   await app.listen(3000);
 }
+
 bootstrap();
